f515d983f8fc4badcb227f4ec2d0c8864b2949b5
ajaegle <mail@ajaegle.de>
2014-08-28 01:27:54 +0200

    get hard-coded user info from github using restangular

--- app/app.js
+++ app/app.js
@@ -12,4 +12,16 @@ angular.module('aj', ['restangular']);
 angular.module('aj')
   .controller('MainCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
     $scope.initialValue = 'asdf';
+    var User = Restangular.allUrl('users', 'https://api.github.com/users');
+    $scope.fetchUser = function (name) {
+      User.one(name).get().then(
+        function (data) {
+          $scope.user = data;
+        },
+        function (err) {
+          $scope.err = err;
+        }
+      )
+    };
+    $scope.fetchUser('ajaegle');
   }]);
