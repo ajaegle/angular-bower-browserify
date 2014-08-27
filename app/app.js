242a74eb8452aaa242722b85a5729ca945f4194c
ajaegle <mail@ajaegle.de>
2014-08-28 01:06:59 +0200

    add angular and restangular respecting dependencies

--- app/app.js
+++ app/app.js
@@ -2,6 +2,14 @@ var printer = require('./printer');
 var $ = require('jquery');
 printer.print();
 printer.print();
-printer.print();
 
 $( ".content" ).append( "<p>Content...</p>" );
+
+var angular = require('angular');
+require('restangular');
+
+angular.module('aj', ['restangular']);
+angular.module('aj')
+  .controller('MainCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
+    $scope.initialValue = 'asdf';
+  }]);
